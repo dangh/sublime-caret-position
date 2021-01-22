bbeb6a5d86dd5ca22b654951dd36826153560950
Dang <huynhminhdang@gmail.com>
2021-01-22 22:02:19 +0800

    Update caret_position.py

--- caret_position.py
+++ caret_position.py
@@ -3,10 +3,13 @@ import sublime_plugin
 
 
 class CaretPositionListener(sublime_plugin.EventListener):
-	def on_selection_modified(self, view):
-		sels = list(view.sel())
-		begin, end = sels[0]
-		text = "Position {}-{}".format(begin, end)
-		if len(sels) > 1:
-			text += "..."
-		view.set_status('z_caret_position', text)
+    def on_selection_modified(self, view):
+        sel = list(view.sel())
+        if sel:
+            begin, end = sel[0]
+            text = "Position: {}".format(begin)
+            if begin != end:
+                text += "-{}".format(end)
+            if len(sel) > 1:
+                text += "..."
+            view.set_status("z_caret_position", text)
